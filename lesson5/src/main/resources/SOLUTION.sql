16359fb9daf9a027a223e56cc8c3ceaf2ab326bf
Максим <Maxim.d.morozov@gmail.com>
2023-07-24 19:11:01 +0400

    done

--- lesson5/src/main/resources/SOLUTION.sql
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -1,6 +1,6 @@
 SELECT * from payment WHERE amount>=500;
 SELECT * from student WHERE DATEADD(YEAR, -20, CURRENT_DATE) > birthday;
-SELECT * from student WHERE groupnumber=10 AND DATEADD(year, 20, birthday) < '20230723';
+SELECT * from student WHERE groupnumber=10 AND DATEADD(YEAR, -20, CURRENT_DATE) < birthday;
 SELECT * from student WHERE name LIKE 'Mike%' OR groupnumber IN (4, 5, 6);
 SELECT * from payment WHERE DATEADD(MONTH, -8, CURRENT_DATE) < payment_date;
 SELECT * from student WHERE name LIKE 'A%';
--- lesson6/src/main/resources/SOLUTION.sql
+++ lesson6/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
 SELECT * FROM payment JOIN paymentType ON paymentType.id = payment.type_id WHERE paymentType.name='MONTHLY';
-SELECT * FROM mark JOIN subject ON subject.id = mark.subject_id WHERE subject.name='art';
+SELECT * FROM mark JOIN subject ON subject.id = mark.subject_id WHERE subject.name='Art';
 SELECT DISTINCT s.id, s.name, s.birthday, s.groupnumber FROM payment AS p JOIN paymenttype AS t ON p.type_id = t.id JOIN student AS s ON p.student_id = s.id WHERE t.name = 'WEEKLY';
 SELECT st.id, st.name, st.birthday, st.groupnumber FROM mark AS m JOIN subject AS s ON s.id = m.subject_id JOIN student AS st ON st.id = m.student_id WHERE s.name = 'Math';
